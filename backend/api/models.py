24011285954f398501f8c108a8070cfd940772a3
harshbhagat69 <harshbhagat787@gmail.com>
2026-02-25 12:16:32 -0500

    created the database

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+# Entry with name  & JSON file
+class HistoricalEntry(models.Model):
+    name = models.CharField(max_length=200)
+    details = models.JSONField() 
+
+    def __str__(self):
+        return self.name
